2ce4da0eca09018a82c366f10e122e61614fe072
yosuabisma <yosuabisma@gmail.com>
2020-04-15 00:18:17 +0700

    improve bottom bar

--- libs/js/si-donar.js
+++ libs/js/si-donar.js
@@ -10,6 +10,49 @@ function removeUsername(){
     localStorage.removeItem("name");
 }
 
+
+function setKey(key){
+    return localStorage.setItem("key", key);
+}
+
+function getKey(){
+    return localStorage.getItem("key");
+}
+
+function removeKey(){
+    localStorage.removeItem("key");
+}
+
+
 function logoutApp(){
     removeUsername();
+    removeKey();
+}
+
+function navigate(html){
+    window.location.href = '../user/'+html+'.html';
+}
+
+function setActiveMenu(menu){
+    var profileKey = getKey();
+    var menuArray = ["home", "stok_darah", "cari_darah"];
+    
+    if(menu !== 'profile'){
+        document.getElementById("profile").src = '../src/img/profile_photo_default/'+profileKey+'.jpg';
+        document.getElementById(menu).src = '../src/img/icons/'+menu+'_active_icon.png';
+        for(var i = 0 ; i < menuArray.length ; i++){
+            if(menuArray[i] != menu){
+               document.getElementById(menuArray[i]).src = '../src/img/icons/'+menuArray[i]+'_icon.png';
+            }
+        }
+    }else{
+        document.getElementById("profile").src = '../src/img/profile_photo_default/'+profileKey+'.jpg';
+        document.getElementById("profile").classList.add("activeProfile");
+        for(var i = 0 ; i < menuArray.length ; i++){
+            if(menuArray[i] != menu){
+               document.getElementById(menuArray[i]).src = '../src/img/icons/'+menuArray[i]+'_icon.png';
+            }
+        }
+    }
+    
 }
