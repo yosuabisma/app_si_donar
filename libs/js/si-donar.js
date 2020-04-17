56846812cc23ae16d81f03029ff5507f03903b00
yosuabisma <yosuabisma@gmail.com>
2020-04-18 02:31:08 +0700

    register page and improve ui

--- libs/js/si-donar.js
+++ libs/js/si-donar.js
@@ -56,3 +56,19 @@ function setActiveMenu(menu){
     }
     
 }
+
+function activeSelectOption(parentId, childClass, active){
+    // Get the container element
+    var btnContainer = document.getElementById(parentId);
+     // Get all buttons with class="btn" inside the container
+     var btns = btnContainer.getElementsByClassName(childClass);
+
+     // Loop through the buttons and add the active class to the current/clicked button
+     for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName(active);
+            current[0].className = current[0].className.replace(` ${active}`, "");
+            this.className += ` ${active}`;
+        });
+     }
+}
